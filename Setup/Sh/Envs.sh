47073665117dfcfbcc311602024c6cc9e6bc0a9d
nkpro2000 <45198561+nkpro2000@users.noreply.github.com>
2022-08-23 19:03:45 +0530

    Update Envs.sh

--- Setup/Sh/Envs.sh
+++ Setup/Sh/Envs.sh
@@ -25,10 +25,18 @@ else
 # [drwxr-xr-x root  root ]  /mnt/Extended/LinuxExtRoot
 # [drwxr-xr-x root  root ]  ├── home
 # [drwx------ nkpro nkpro]  │   └── nkpro
-# [drwxrwxrwt root  root ]  └── thome
-# [drwx------ nkpro nkpro]      ├── nkpro_darling
-# [drwx------ nkpro nkpro]      ├── nkpro_droid
-# [drwx------ nkpro nkpro]      └── nkpro_wine
+# [drwxrwxrwt root  root ]  ├── thome
+# [drwx------ nkpro nkpro]  │   ├── nkpro_darling
+# [drwxr-xr-x nkpro nkpro]  │   │   ├── darling_src_repo
+# [-rw-r--r-- nkpro nkpro]  │   │   └── init.sh
+# [drwx------ nkpro nkpro]  │   ├── nkpro_droid
+# [drwx------ nkpro nkpro]  │   └── nkpro_wine
+# [drwxrwxrwt root  root ]  ├── tusr
+# [drwxr-xr-x nkpro nkpro]  │   ├── nkpro_darling
+# [drwxr-xr-x nkpro nkpro]  │   └── nkpro_droid
+# [drwxr-xr-x root  root ]  └── var
+# [drwxr-xr-x root  root ]      └── cache
+# [drwxr-xr-x root  root ]          └── pacman
 
 #darling{
 export DPREFIX="/mnt/Extended/LinuxExtRoot/thome/${USER}_darling/.darling"
